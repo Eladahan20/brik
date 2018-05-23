daee308af6624651e215dedf44afa311a36367af
Elad Dahan <eladdahan20@gmail.com>
2018-05-23 15:25:01 +0300

    test

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
-// API file for interacting with MongoDB
-const api = require('./server/routes/api');
-
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -14,8 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false}));
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// API location
-app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
